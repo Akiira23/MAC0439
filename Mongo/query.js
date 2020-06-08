2c082509f944c299f9236c88d2d3c9984ab8e19a
rockrick1 <henriquecerquinho@gmail.com>
2020-06-08 19:16:55 -0300

    add queries file

--- Mongo/query.js
+++ Mongo/query.js
@@ -4,5 +4,19 @@ DBQuery.shellBatchSize = 100;
 // encontra todas as imagens dos torneios de overwatch
 db.torneios.find({"esporte":"overwatch"},{"imagens":1, "_id":0}).sort({esporte:1}).pretty();
 
-// encontra todas os times com mulheres como treinadoras
-db.times.find({"treinador.genero":"feminino"},{"nome":1, "_id":0}).pretty();
+// encontra os times participantes de partidas que empataram
+
+
+// encontra os times que venceram pelo menos uma partida em um torneio dado
+
+
+// encontra todos os jogadores que ja jogaram em uma partida de mais de 2h
+
+
+// encontra os treinadores de times que já ganharam em menos de 1h20
+
+
+// encontra os jogadores de mais de 40 anos que ja ganharam uma partida
+
+
+// encontra todos os jogadores que ja jogaram em uma partida de mais de 2h
