5e71aa3e0aec81871847829021fc7df95be5ebb2
Henrique Cerquinho <henriquecerquinho@gmail.com>
2020-06-08 18:46:01 -0300

    Add session script

---
+++ Mongo/insertSessoes.js
@@ -0,0 +1,504 @@
+use FantasyDreams;
+DBQuery.shellBatchSize = 400;
+db.sessoes.insertMany([
+  {
+    "usuario": "54ffea2d-ea9d-4b2b-a42b-8554c0fbbd39",
+    "status": "ocupado",
+    "chats": "ca98347c-8a25-46eb-a4e9-13222c3a5322"
+  },
+  {
+    "usuario": "94492c2c-9ff0-4066-b2a2-523ddf38b286",
+    "status": "online",
+    "chats": "4eac4b0a-0a26-4378-b517-a0cfd59f0bfa"
+  },
+  {
+    "usuario": "67e4a558-5541-462f-9873-83ee9f88ec01",
+    "status": "offline",
+    "chats": "13df1da6-d229-444a-86e3-cc106d0e08dc"
+  },
+  {
+    "usuario": "bc7b6b8b-bda9-4517-8122-c709ea5ae967",
+    "status": "offline",
+    "chats": "a16558d5-c3f7-46fe-b838-b39763192525"
+  },
+  {
+    "usuario": "7725e281-3c11-4c96-acfb-5bb26ca37d9b",
+    "status": "ausente",
+    "chats": "77dedc1a-1362-4dfc-8044-a915997688fe"
+  },
+  {
+    "usuario": "c2354fb4-4dda-41be-8574-b804af2016cf",
+    "status": "ocupado",
+    "chats": "967e5f8b-f5c7-4721-b018-42623600d3ba"
+  },
+  {
+    "usuario": "ff7685e2-826c-4f01-b539-2b29563b0958",
+    "status": "offline",
+    "chats": "02662d71-89e7-425c-8def-ec357c765050"
+  },
+  {
+    "usuario": "e5065a1a-aeef-4a61-b48f-31ea08816cb1",
+    "status": "invisivel",
+    "chats": "b66a9dea-5a9b-496b-b58d-d76003bd6aea"
+  },
+  {
+    "usuario": "c4b29c29-ebd6-40ab-9b7b-eb5f71a235dd",
+    "status": "invisivel",
+    "chats": "e7ea9ce0-9be4-4e38-bc05-b04ca18a880f"
+  },
+  {
+    "usuario": "1a5a6d96-0419-4f80-8673-1eb3d0bacfbe",
+    "status": "invisivel",
+    "chats": "3bb8c07a-aaa4-47e6-9973-bdb926f83e3e"
+  },
+  {
+    "usuario": "821ab71d-7bc4-452d-82f6-f0926ddc40b3",
+    "status": "ausente",
+    "chats": "9a037321-d44f-42f4-8cf5-691d732d3042"
+  },
+  {
+    "usuario": "99a18add-2847-43e8-a368-eef493a092c8",
+    "status": "invisivel",
+    "chats": "53c89b7d-4ac6-4975-9974-c60127728c9a"
+  },
+  {
+    "usuario": "8868e880-c78f-48ad-a2aa-a048dfaf19fb",
+    "status": "online",
+    "chats": "9cf7fdeb-49c2-44b7-8395-538348a9e8a9"
+  },
+  {
+    "usuario": "e44c323b-e1e2-4a70-b5e4-1afde09f76b2",
+    "status": "ocupado",
+    "chats": "00abdc10-e402-458a-a940-3b1190227a52"
+  },
+  {
+    "usuario": "0c76dddb-aa45-417e-8c5d-e9dc30adaa46",
+    "status": "ausente",
+    "chats": "94949a96-c407-4e97-ba32-a55c213affc1"
+  },
+  {
+    "usuario": "d1eec431-5b31-4652-9fb7-8963f556778e",
+    "status": "online",
+    "chats": "8bbe430d-a4e1-438d-90c9-1ad2fd59cec4"
+  },
+  {
+    "usuario": "9e6f3a69-cb3a-4fc4-b96f-a3ffc927c7b2",
+    "status": "invisivel",
+    "chats": "ef994e3f-591c-4658-8ea4-7445505553dc"
+  },
+  {
+    "usuario": "d0bbf7b4-ae8e-4c54-afd4-62cd20631a06",
+    "status": "offline",
+    "chats": "4d8c79e8-ed03-4fa6-a162-cad9df35f184"
+  },
+  {
+    "usuario": "621d00df-22c5-47b0-9330-83620ffeae6c",
+    "status": "offline",
+    "chats": "444cd75c-2d6a-4314-a8fa-b8ab7e9a5ae3"
+  },
+  {
+    "usuario": "b11e0172-4153-48fa-a973-9b8d44a4f38a",
+    "status": "ocupado",
+    "chats": "7e934817-b0d6-4eb8-a005-7ad75330ac8f"
+  },
+  {
+    "usuario": "da85476a-5099-4c7e-a5d5-7b3ed37865c7",
+    "status": "online",
+    "chats": "323cb645-44ac-4d27-bdf3-30374971b25f"
+  },
+  {
+    "usuario": "21396ef2-9c9a-46b9-bac1-fc1659d5e720",
+    "status": "online",
+    "chats": "08a288f7-7f88-4f95-b031-1db3a21e6386"
+  },
+  {
+    "usuario": "f1dca5d5-bba9-435f-b3c6-c4dabec8f249",
+    "status": "ocupado",
+    "chats": "09b8ecee-d9bc-4c05-bf75-9ba88664f13a"
+  },
+  {
+    "usuario": "45c7c6b5-924c-4858-979a-404b11a6b10c",
+    "status": "ausente",
+    "chats": "3c158555-8ddf-4a50-8055-6e38bbf12c4a"
+  },
+  {
+    "usuario": "f07d72ef-74a2-4e98-96de-bf76fc66b35e",
+    "status": "ausente",
+    "chats": "18f70f42-a03f-496c-8ba7-c8fbdf893af2"
+  },
+  {
+    "usuario": "ba44e21b-2122-4360-9d1c-d193ae82928c",
+    "status": "ausente",
+    "chats": "509cdd27-1ed0-4eeb-a660-f7e7caf01a8d"
+  },
+  {
+    "usuario": "3e62491c-0f08-4544-95a6-8f03731fa12d",
+    "status": "online",
+    "chats": "f85a04f6-fa5b-43df-873c-0f83d0f1bfe3"
+  },
+  {
+    "usuario": "0cda93fe-369b-44b9-a107-11eb2258e931",
+    "status": "invisivel",
+    "chats": "d6b02421-d7c6-4076-af25-9dafb1ebf424"
+  },
+  {
+    "usuario": "25c24f2a-cbaf-43b7-aa73-a2fe8aa39355",
+    "status": "ocupado",
+    "chats": "ebb762c2-0739-41e1-b7a3-730563e923a3"
+  },
+  {
+    "usuario": "c950ab52-bbde-49b6-b988-9808fcddc595",
+    "status": "invisivel",
+    "chats": "4556e6a3-c898-44ca-b333-871f0d07d2ad"
+  },
+  {
+    "usuario": "2371e982-121c-4d8f-9c16-ddde0e129229",
+    "status": "ausente",
+    "chats": "bad17d8e-1974-404c-9ec6-967b0438fc52"
+  },
+  {
+    "usuario": "c6be9d4b-6a75-4efd-b4d5-d8930c0c1f4b",
+    "status": "invisivel",
+    "chats": "21bc7982-2be1-43b3-987f-e29677986c8f"
+  },
+  {
+    "usuario": "20083f76-7b80-42ed-96c9-9de5f9c1521e",
+    "status": "offline",
+    "chats": "17534eb8-da5e-4a27-ac19-9fca9507a221"
+  },
+  {
+    "usuario": "e0d872a8-1105-4213-8d14-315c623b2b3a",
+    "status": "ausente",
+    "chats": "67dab205-6ba2-4622-b8e4-5a5d427caeee"
+  },
+  {
+    "usuario": "7107cc80-ab97-4425-b4b0-e43e516269ec",
+    "status": "offline",
+    "chats": "d8c1f585-2a9e-4fd2-b28a-4b0b1d938364"
+  },
+  {
+    "usuario": "edd86b29-ff11-46fd-97b0-1e3b77ea4ac7",
+    "status": "invisivel",
+    "chats": "d09b4d5f-b419-47e3-8f56-f42da755e9b1"
+  },
+  {
+    "usuario": "f24eddf6-2f32-410a-921a-03108c8fcf3e",
+    "status": "offline",
+    "chats": "1dd31bd8-2a3c-4512-b7a5-96cc4c5e7301"
+  },
+  {
+    "usuario": "12ba2e20-d26b-48f0-9ae6-50b8b1dae652",
+    "status": "ausente",
+    "chats": "b77e1594-9075-4005-9dee-a8a87e5ae5fa"
+  },
+  {
+    "usuario": "b0df7294-2e64-4596-bd70-073f59327412",
+    "status": "offline",
+    "chats": "d28b43a3-8209-40ce-8dc4-583356a1e509"
+  },
+  {
+    "usuario": "df4e474e-4031-4c62-9c48-4ac9328b8453",
+    "status": "offline",
+    "chats": "588f1295-ed33-472c-aa4f-60edac7d59ff"
+  },
+  {
+    "usuario": "0b12d14c-8bad-4b84-a7ed-8fec10fb8931",
+    "status": "invisivel",
+    "chats": "e9e07c3f-b694-4e54-ba22-ac8be3d51139"
+  },
+  {
+    "usuario": "73dc1972-06e9-4c55-85bc-552a93907ff9",
+    "status": "ocupado",
+    "chats": "c5da5edd-2374-4d3f-afc2-b755bdd5c7cf"
+  },
+  {
+    "usuario": "af7bb510-3c9f-4902-a49f-54d12a0e5822",
+    "status": "offline",
+    "chats": "5baa7e88-6cd8-47c7-8387-7bc6a4ce8a9d"
+  },
+  {
+    "usuario": "6579bedf-5c46-4608-8c1a-0baf70de816e",
+    "status": "ocupado",
+    "chats": "7c2e5d58-140c-4338-ada6-6967fc311add"
+  },
+  {
+    "usuario": "a4cd62fb-ee7d-4c41-9bad-6b3e1a6e9e60",
+    "status": "ocupado",
+    "chats": "1ddb126b-cad6-4fc3-b120-94a2855626cf"
+  },
+  {
+    "usuario": "0d3d92ff-c607-496d-af3b-417370dba99d",
+    "status": "ocupado",
+    "chats": "588d2d65-8bfe-451c-b5c3-ee9cee56396b"
+  },
+  {
+    "usuario": "21534b45-1002-4cc4-975a-76f44bcfcb35",
+    "status": "ocupado",
+    "chats": "9934eb85-270e-4071-b493-9fe6a0e9b08c"
+  },
+  {
+    "usuario": "340d4e48-68b8-4b08-91eb-ac2c569afe03",
+    "status": "ocupado",
+    "chats": "15d5718b-50ae-462d-b8ae-62f8e859e486"
+  },
+  {
+    "usuario": "98d6a6bb-a879-4162-b629-6d31b5403813",
+    "status": "offline",
+    "chats": "e250a2a8-533a-48f0-bb1b-06e5fff747c4"
+  },
+  {
+    "usuario": "b4593909-7f8d-48fa-a28a-ed00a6cb42c0",
+    "status": "ausente",
+    "chats": "36ba4c11-2249-47b0-86a0-4b75076518a4"
+  },
+  {
+    "usuario": "3dbaf2bd-c798-4adb-9d35-0bf9552f6ed5",
+    "status": "invisivel",
+    "chats": "de0988d0-87d2-4701-9515-cef0abfe079d"
+  },
+  {
+    "usuario": "45167d95-6052-48cf-8281-86b6fa031931",
+    "status": "offline",
+    "chats": "bb426449-8ff8-4d4e-91d4-85b70f946afa"
+  },
+  {
+    "usuario": "e9f88243-d770-4302-94a1-bb45f800bd79",
+    "status": "ocupado",
+    "chats": "9e285406-73ed-4eb7-b7a9-f14281f6d9d8"
+  },
+  {
+    "usuario": "ce49cc94-44d0-4f58-b45c-b9ccfa4f5bd4",
+    "status": "ocupado",
+    "chats": "d27e544b-98ba-46ba-a1f9-2904ad7b6c3e"
+  },
+  {
+    "usuario": "737f9dde-eb8a-4e43-a649-0f9b1944262e",
+    "status": "ausente",
+    "chats": "d29573d9-a77c-474f-aea7-ba1b4cdda65b"
+  },
+  {
+    "usuario": "e90710fe-2be5-485a-9ec9-7019ceddba7f",
+    "status": "ocupado",
+    "chats": "8eaefa0b-8aba-4a7a-94f1-67f44fe18184"
+  },
+  {
+    "usuario": "2a56697f-aeb2-4a6b-92a0-8a30b926c67c",
+    "status": "ocupado",
+    "chats": "0644d7c7-4ed9-4d77-a5bf-8cf871328239"
+  },
+  {
+    "usuario": "e793c1eb-f956-4ce6-b0e3-c4067dbcad6e",
+    "status": "ocupado",
+    "chats": "d5da7252-efb6-4501-a434-ed157521271e"
+  },
+  {
+    "usuario": "8559c6a0-3e0a-4664-a333-2d7efb58718a",
+    "status": "online",
+    "chats": "d1eb75e7-8234-48e2-b3ee-cbd8063d8dd1"
+  },
+  {
+    "usuario": "7bd7798c-7ca6-4625-9f70-bd694371b148",
+    "status": "online",
+    "chats": "80ebdd9f-8cbc-463f-9877-6c8e708b2a0f"
+  },
+  {
+    "usuario": "f40623a9-c5f3-48fb-883f-d777fa88c79a",
+    "status": "online",
+    "chats": "c1e15973-0332-4a32-be63-a16cabdd6eea"
+  },
+  {
+    "usuario": "5544d9ad-df8e-4436-aac5-d2bc533509ea",
+    "status": "ocupado",
+    "chats": "f9c325a1-b03e-49ee-83e7-3174de7ee4a9"
+  },
+  {
+    "usuario": "d73234e3-b989-4025-bdde-92dfb638d5d2",
+    "status": "online",
+    "chats": "6ff9142e-beb9-4c72-b421-12ac76c86506"
+  },
+  {
+    "usuario": "e068c2f0-fbec-4791-a396-9e784774ba30",
+    "status": "ocupado",
+    "chats": "50bfdb91-b62c-40e1-8fe2-62c2b4166f32"
+  },
+  {
+    "usuario": "1c497c8d-783e-4623-82aa-68211cca160e",
+    "status": "offline",
+    "chats": "1c9d08c6-a19b-422a-8ba3-5705fbefdb45"
+  },
+  {
+    "usuario": "2a2c3c8c-7fb7-4c6b-94be-cfd7a8904dd3",
+    "status": "ausente",
+    "chats": "3e1b81f7-733b-4e98-ac36-dc009aaf7570"
+  },
+  {
+    "usuario": "f191049f-8ec6-4667-a62f-5a988aba979c",
+    "status": "invisivel",
+    "chats": "1fe1943e-fc97-4c87-80c2-43c846108a34"
+  },
+  {
+    "usuario": "dccd77b1-7101-44f6-9c03-fdc7e5216339",
+    "status": "online",
+    "chats": "b7371b27-08c4-4e2d-a8fc-83946e2b23a7"
+  },
+  {
+    "usuario": "dbc565cc-44a3-453d-a1d0-d8745de88c81",
+    "status": "online",
+    "chats": "4ce0d030-cd8c-4b8e-a6cf-f03474ad968f"
+  },
+  {
+    "usuario": "925493bd-f10a-404f-9e58-07137f084061",
+    "status": "invisivel",
+    "chats": "b07ecc8b-21cb-4ed0-940c-b55e4b81d8d4"
+  },
+  {
+    "usuario": "8cb3c947-0106-4488-b03f-fc4d9325a729",
+    "status": "ausente",
+    "chats": "794c7c6f-ab93-49d2-90fd-55dbcaa2f967"
+  },
+  {
+    "usuario": "73e6adc5-b410-47f7-89a7-3fbd125f6cee",
+    "status": "ausente",
+    "chats": "ebbfa848-c318-4e7f-8fbe-8c9d90dcfae2"
+  },
+  {
+    "usuario": "4af58d8a-b04f-4dca-b666-23885d8f3390",
+    "status": "invisivel",
+    "chats": "d9221b07-ec39-4cbc-bee6-e16e47d5843a"
+  },
+  {
+    "usuario": "1d6193d3-5bfc-41d6-b021-ce7a70421f92",
+    "status": "online",
+    "chats": "f5b8c0a1-6845-44c9-a7ea-fde6f213ee4c"
+  },
+  {
+    "usuario": "99469f1b-f0e6-4930-8b03-ba924e415f03",
+    "status": "ausente",
+    "chats": "42dfaf2b-b39c-4527-be77-6ede9bc88713"
+  },
+  {
+    "usuario": "d5734dbf-6a2c-4c5b-baf8-6f079b5f56de",
+    "status": "ocupado",
+    "chats": "6e1b212b-acca-4af2-8d15-2b4f55481584"
+  },
+  {
+    "usuario": "5c8e8970-ab55-4d12-9386-7dc4fc985968",
+    "status": "offline",
+    "chats": "5148b7fb-f21b-4dd7-98c2-be367f333ef5"
+  },
+  {
+    "usuario": "9902291c-b568-424a-b784-0d025285083a",
+    "status": "offline",
+    "chats": "d3c3fbb5-bd04-41c6-bbb7-d249735f2b76"
+  },
+  {
+    "usuario": "faa2a6e8-c6fd-4e95-927d-c9513a9db471",
+    "status": "ocupado",
+    "chats": "5ab3173c-41d8-42a2-8f99-9c8185532cf0"
+  },
+  {
+    "usuario": "2a4b0845-ccca-4f26-9319-16006df2ab33",
+    "status": "online",
+    "chats": "4df1c77d-5fe8-4afa-b3c9-74c53795fb90"
+  },
+  {
+    "usuario": "37f30f47-d655-4167-831b-38d6dfdf5e0f",
+    "status": "ocupado",
+    "chats": "3b70e095-e513-4343-b06d-8d4f49738160"
+  },
+  {
+    "usuario": "22a805fb-c388-4bbd-a4ba-c89ad989afea",
+    "status": "ausente",
+    "chats": "c8c0e0e9-b51d-4a92-972b-52eea4da821b"
+  },
+  {
+    "usuario": "2e240279-f8a5-4061-a9d1-9a5f87599590",
+    "status": "ausente",
+    "chats": "b3800345-2d08-4abc-b7c0-888880b4535a"
+  },
+  {
+    "usuario": "dd3e267f-11e7-4a76-aaa6-e6428f062af1",
+    "status": "ausente",
+    "chats": "1e951fed-4188-443f-97ab-8ee282940ae3"
+  },
+  {
+    "usuario": "1e516354-47e8-4a1a-8472-cc18cd68d0fe",
+    "status": "offline",
+    "chats": "938bbeb5-c778-4d74-9e05-c37dddb8d91b"
+  },
+  {
+    "usuario": "e6ba225f-a8f7-4340-a92d-0d0293552640",
+    "status": "ocupado",
+    "chats": "3f57af35-280a-4b7f-97b6-8e14bf9ef46d"
+  },
+  {
+    "usuario": "67a590e7-a297-46b3-9c9d-83618a8bb3f5",
+    "status": "invisivel",
+    "chats": "e4792281-0101-4549-825e-63c7ec4051af"
+  },
+  {
+    "usuario": "3af7f917-e49a-419e-9ef1-e8cd48ebd2cb",
+    "status": "invisivel",
+    "chats": "3699c0f4-41c8-4889-b4e6-be68cacfab17"
+  },
+  {
+    "usuario": "6a5b098d-b666-47a7-89da-95e98e7c1ec0",
+    "status": "offline",
+    "chats": "3bfd2de4-1547-4f38-8484-162a5fba4607"
+  },
+  {
+    "usuario": "6425873c-01b9-4571-bb7d-707e8fb2f191",
+    "status": "ocupado",
+    "chats": "f430e1fa-b9dc-44ab-b7a1-a92713dfdcc1"
+  },
+  {
+    "usuario": "d64670bf-ee36-4e97-8b24-5f5001abbf60",
+    "status": "online",
+    "chats": "ff0f6ea6-1233-4619-9b33-95d7086748ca"
+  },
+  {
+    "usuario": "407b379c-8422-4353-9042-d40a5e75c7fa",
+    "status": "ocupado",
+    "chats": "6f30657c-d553-4925-9e7d-7102df94d7c9"
+  },
+  {
+    "usuario": "3e56d579-32fb-43e4-b576-641d4c3f7e5c",
+    "status": "invisivel",
+    "chats": "012f9d41-e99d-4977-8c86-5ff034c129c4"
+  },
+  {
+    "usuario": "0de4374b-6316-4d53-8f0a-d51470d84c5f",
+    "status": "invisivel",
+    "chats": "20a6a165-37ed-4fef-aeae-baf2d09bc58a"
+  },
+  {
+    "usuario": "6fad987a-da15-446c-9bfa-fd2a891302d8",
+    "status": "online",
+    "chats": "5ef1bb14-c1d7-4e5d-bed6-34f84e9b5ea4"
+  },
+  {
+    "usuario": "a122d118-db79-475d-9698-fcd1121c8968",
+    "status": "ausente",
+    "chats": "b631f01e-96f0-4aec-8552-20a511eab106"
+  },
+  {
+    "usuario": "cb95e85e-301d-48bb-84ff-9432b12b9d8e",
+    "status": "invisivel",
+    "chats": "b5da1929-850f-4281-b56d-5d15a06e57bf"
+  },
+  {
+    "usuario": "a2e3148b-91e2-450c-8ad2-c3f746c3002b",
+    "status": "ocupado",
+    "chats": "fe7245a6-02c6-4751-b09c-c7d54ad6ffe7"
+  },
+  {
+    "usuario": "e010e2ec-e144-43be-b2e6-ac17815b5373",
+    "status": "online",
+    "chats": "383e3595-12ca-47ae-b780-9f9f22308ba3"
+  },
+  {
+    "usuario": "a7b5744f-841f-42ba-9b7e-c0969490e4eb",
+    "status": "ausente",
+    "chats": "2a8b2cb7-4309-4727-851c-45a58a308a42"
+  }
+]);
