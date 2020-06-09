551b8fda64b0cdd84a243617ba56bc763e515677
Mateus dos Anjos <mateus.a.anjos@gmail.com>
2020-06-09 19:01:51 -0300

    Add mongo query

--- Mongo/query.js
+++ Mongo/query.js
@@ -4,8 +4,14 @@ DBQuery.shellBatchSize = 100;
 // encontra todas as imagens dos torneios de overwatch OK
 db.torneios.find({"esporte":"overwatch"},{"imagens":1, "_id":0}).sort({esporte:1}).pretty();
 
-// encontra os times participantes de partidas que empataram
-
+// encontra os times participantes de partidas que empataram OK
+db.partidas.aggregate([
+    {$project: {times: 1, placar_str: {$split: ["$placar", "-"]}}},
+    {$project: {time_1: {$arrayElemAt: ["$times", 0]}, time_2: {$arrayElemAt: ["$times", 1]}, pontos_1: {$arrayElemAt: ["$placar_str", 0]}, pontos_2: {$arrayElemAt: ["$placar_str", 1]}}},
+    {$project: {resp: {$eq: ["$pontos_1", "$pontos_2"]}, nome_time_1: "$time_1.nome", nome_time_2: "$time_2.nome", _id: 0}},
+    {$match: {resp: true}}, 
+    {$project: {resp: 0}},
+    ]);
 
 // encontra os times que venceram pelo menos uma partida em um torneio dado
 
@@ -24,3 +30,6 @@ db.partidas.find({"duracao": /^0h|^1h[0-1][0-9]m/i},{"duracao":1,"times.treinado
 db.partidas.find({"duracao":/^[2-9]/i},{"duracao":1,"times.participantes":1, "_id":0}).sort({duracao:1}).pretty();
 
 // encontra todos os jogadores que ja jogaram em uma partida de mais de 3h
+
+
+
