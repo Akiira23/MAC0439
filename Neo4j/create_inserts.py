0fa0dc9fc20894b6e9fad2db8b25e4062b4af7e5
Akiira23 <38569716+Akiira23@users.noreply.github.com>
2020-06-10 18:55:33 -0300

    delete .py

--- Neo4j/create_inserts.py
+++
@@ -1,6 +0,0 @@
-def main():
-    file = open("usuarios.txt", "r")
-    for line in file:
-        splited_line = line.split("/")
-        print("CREATE (user_%s:User {id:%s, nome:'%s', email:'%s'})" %(splited_line[0], splited_line[0], splited_line[1], splited_line[2]))
-main()
